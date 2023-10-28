0ede8597f7b0ca55bff7124be707eab2fefeea6e
Christine Campbell <christiecamp@Christines-MacBook-Pro.local>
2023-10-28 14:44:11 -0400

    updating index.js, file paths, and starting basic function coding

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 const inquirer = require('inquirer');
 const jest = require('jest');
 
+//will apply 'fs' when creating the svg code 
+const fs = require ('fs');
+
+//creating a seperate file for user input
+const input = require('./lib/input');
+
 
 //function to create logo
-
+function lazyLogo(output) {};
 
 //function to initialize - init()
+function hands() {
+    inquirer
+    .prompt(input)
+    .then((output) => {
+        lazyLogo(output);
+    })
+    .catch(err => {
+        console.log(err)
+    });
+};
 
-//init ()
+hands();
