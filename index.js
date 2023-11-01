48ddf8ba905923723604817da5fd74ce5bcd5341
Christine Campbell <christiecamp@Christines-MacBook-Pro.local>
2023-11-01 15:59:20 -0400

    updating shapes and final testing for validations

--- index.js
+++ index.js
@@ -10,10 +10,14 @@ const logoShape = require('./lib/shapes/logoShape.js');
 //function to create logo
 function lazyLogo(output) {
     const svg = logoShape (output); //generated shape - logoShape.js
-    fs.writeFile(svgPath, svg, (err) => {
-    if (err) throw new Error(err);
-    console.log('Your SVG Logo Generated! Check it out in the sample folder!');
-    });
+        fs.writeFile(svgPath, svg, (err) => {
+            if (err) throw new Error(err);
+                console.log('Your SVG Logo Generated! Check it out in the sample folder!');
+        });
+        // fs.open(html, (err) => {
+        //     if (err) throw new Error(err);
+        //         console.log('Your SVG Logo can be viewed in your open browser!');
+        // }); 
 };
 
 //function to initialize
